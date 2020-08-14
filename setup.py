cb05ad35ca9d6a591e3166c4d04de3974ddeaac1
Swarup Ghosh <snwg@live.com>
2020-08-14 22:55:51 +0530

    Minor change to setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open("requirements.txt") as req_file:
-    requirements = req_file.read().strip().split('\n')
-
 setuptools.setup(
     name="keras-telegram-bot",
     version="0.1.0",
@@ -15,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/swghosh/keras-telegram-bot",
-    packages=requirements,
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
